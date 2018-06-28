c3964248a7f3cb9940beaa5f74910423fd0d1b47
Taenys <nanshee4@gmail.com>
2018-06-28 16:33:17 +0200

    initial commit prijet biblio

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Bibli\'O Web' });
+});
+
+module.exports = router;
